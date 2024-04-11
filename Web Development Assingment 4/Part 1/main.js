5a1910361d1f6841e5c7f06deddce3eb3f312c4a
The-Holy-Wooomy <ethan.hyde@dcmail.ca>
2024-04-11 17:46:46 -0400

    Assignment 4

--- Web Development Assingment 4/Part 1/main.js
+++ Web Development Assingment 4/Part 1/main.js
@@ -17,7 +17,7 @@ const insertX = ['Willy the Goblin', 'Big Daddy', 'Father Christmas']
 
 const insertY = ['the soup kitchen', 'Disneyland', 'the White House']
 
-const nsertZ = ['spontaneously combusted', 'melted into a puddle on the sidewalk', 'turned into a slug and crawled away']
+const insertZ = ['spontaneously combusted', 'melted into a puddle on the sidewalk', 'turned into a slug and crawled away']
 
 //3. EVENT LISTENER AND PARTIAL FUNCTION DEFINITION
 
